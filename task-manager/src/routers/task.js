81982cad0b67115b92f966a3e80fcdf9c5432881
VeXuZ <vexuzdorotan@gmail.com>
2020-06-01 13:28:02 +0800

    Logout Sessions

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -46,10 +46,14 @@ router.patch('/tasks/:id', async (req, res) => {
   }
 
   try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    const task = await Task.findById(req.params.id);
+    reqKeys.forEach((key) => (task[reqKeys] = req.body[reqKeys]));
+    await task.save();
+
+    // const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+    //   new: true,
+    //   runValidators: true,
+    // });
 
     if (!task) {
       return res.status(404).send();
